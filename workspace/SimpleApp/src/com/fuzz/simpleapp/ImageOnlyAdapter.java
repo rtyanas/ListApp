840a1e283466809a21371c88446d239cbb7a332e
rtyanas <rtyanas2000@yahoo.com>
2014-01-24 14:14:45 -0500

    Remove unused imports

--- workspace/SimpleApp/src/com/fuzz/simpleapp/ImageOnlyAdapter.java
+++ workspace/SimpleApp/src/com/fuzz/simpleapp/ImageOnlyAdapter.java
@@ -1,18 +1,11 @@
 package com.fuzz.simpleapp;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import com.fuzz.simpleapp.MainActivity.SectionFragment;
 
 public class ImageOnlyAdapter extends ArrayAdapter<String> {
 
